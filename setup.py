d3c5c94bcf1b7d34edf96809be72a1d43250fc9f
ankit62073 <ankitsingh62073@gmail.com>
2024-08-28 11:58:52 +0530

    data added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Customer Chatbot',
+    version= '0.0.0',
+    author= 'Ankit Singh',
+    author_email= 'ankitsingh62073@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
